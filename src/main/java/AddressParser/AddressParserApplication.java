7614d5688ec211eb077dae44ca69ebfcc738d77c
Konstantin <davydenko_k_85@mail.ru>
2022-01-05 10:10:01 +0800

    add sorted countHous

--- src/main/java/AddressParser/AddressParserApplication.java
+++ src/main/java/AddressParser/AddressParserApplication.java
@@ -1,12 +1,7 @@
 package AddressParser;
 
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
-
 import static AddressParser.UtilClass.*;
 
 
--- src/main/java/AddressParser/UtilClass.java
+++ src/main/java/AddressParser/UtilClass.java
@@ -2,10 +2,7 @@ package AddressParser;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
